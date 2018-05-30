b7f478a81c78c440821f476ba097788b3de1cb02
Ryan Andri <ryanandri@linuxmail.org>
2018-05-30 18:46:46 +0700

    SQUASH: Revert "fs: f2fs latest changes"
    * reverts to avoid conflict merge with ALS.
    80b5bf5 UPSTREAM: f2fs: avoid fsync() failure caused by EAGAIN in writepage()
    230098d UPSTREAM: f2fs: clear PageError on writepage - part 2
    16e524b f2fs: check cap_resource only for data blocks
    abd806b Revert "f2fs: introduce f2fs_set_page_dirty_nobuffer"
    5d635f2 f2fs: clear PageError on writepage
    9d05850 f2fs: call unlock_new_inode() before d_instantiate()
    e2a7703 f2fs: refactor read path to allow multiple postprocessing steps
    b405b22 FROMLIST: fscrypt: allow synchronous bio decryption
    8952fb2 f2fs/fscrypt: updates to v4.17-rc1
    b7d01e6 f2fs: updates on v4.16-rc1
    7d2b893 fscrypt: updates on 4.15-rc4
    
    Change-Id: I82ef530a126f561cc2fe8f2ab9a525180577aafc

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -189,20 +189,6 @@ out:
 	return err;
 }
 
-static void recover_inline_flags(struct inode *inode, struct f2fs_inode *ri)
-{
-	if (ri->i_inline & F2FS_PIN_FILE)
-		set_inode_flag(inode, FI_PIN_FILE);
-	else
-		clear_inode_flag(inode, FI_PIN_FILE);
-	if (ri->i_inline & F2FS_DATA_EXIST)
-		set_inode_flag(inode, FI_DATA_EXIST);
-	else
-		clear_inode_flag(inode, FI_DATA_EXIST);
-	if (!(ri->i_inline & F2FS_INLINE_DOTS))
-		clear_inode_flag(inode, FI_INLINE_DOTS);
-}
-
 static void recover_inode(struct inode *inode, struct page *page)
 {
 	struct f2fs_inode *raw = F2FS_INODE(page);
@@ -219,16 +205,13 @@ static void recover_inode(struct inode *inode, struct page *page)
 
 	F2FS_I(inode)->i_advise = raw->i_advise;
 
-	recover_inline_flags(inode, raw);
-
 	if (file_enc_name(inode))
 		name = "<encrypted>";
 	else
 		name = F2FS_INODE(page)->i_name;
 
-	f2fs_msg(inode->i_sb, KERN_NOTICE,
-		"recover_inode: ino = %x, name = %s, inline = %x",
-			ino_of_node(page), name, raw->i_inline);
+	f2fs_msg(inode->i_sb, KERN_NOTICE, "recover_inode: ino = %x, name = %s",
+			ino_of_node(page), name);
 }
 
 static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head,
@@ -237,9 +220,6 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head,
 	struct curseg_info *curseg;
 	struct page *page = NULL;
 	block_t blkaddr;
-	unsigned int loop_cnt = 0;
-	unsigned int free_blocks = sbi->user_block_count -
-					valid_user_blocks(sbi);
 	int err = 0;
 
 	/* get node pages in the current segment */
@@ -292,17 +272,6 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head,
 		if (IS_INODE(page) && is_dent_dnode(page))
 			entry->last_dentry = blkaddr;
 next:
-		/* sanity check in order to detect looped node chain */
-		if (++loop_cnt >= free_blocks ||
-			blkaddr == next_blkaddr_of_node(page)) {
-			f2fs_msg(sbi->sb, KERN_NOTICE,
-				"%s: detect looped node chain, "
-				"blkaddr:%u, next:%u",
-				__func__, blkaddr, next_blkaddr_of_node(page));
-			err = -EINVAL;
-			break;
-		}
-
 		/* check next segment */
 		blkaddr = next_blkaddr_of_node(page);
 		f2fs_put_page(page, 1);
@@ -423,7 +392,7 @@ truncate_out:
 }
 
 static int do_recover_data(struct f2fs_sb_info *sbi, struct inode *inode,
-					struct page *page)
+					struct page *page, block_t blkaddr)
 {
 	struct dnode_of_data dn;
 	struct node_info ni;
@@ -434,7 +403,7 @@ static int do_recover_data(struct f2fs_sb_info *sbi, struct inode *inode,
 	if (IS_INODE(page)) {
 		recover_inline_xattr(inode, page);
 	} else if (f2fs_has_xattr_block(ofs_of_node(page))) {
-		err = recover_xattr_data(inode, page);
+		err = recover_xattr_data(inode, page, blkaddr);
 		if (!err)
 			recovered++;
 		goto out;
@@ -587,7 +556,7 @@ static int recover_data(struct f2fs_sb_info *sbi, struct list_head *inode_list,
 				break;
 			}
 		}
-		err = do_recover_data(sbi, entry->inode, page);
+		err = do_recover_data(sbi, entry->inode, page, blkaddr);
 		if (err) {
 			f2fs_put_page(page, 1);
 			break;
